a94bbd6d19aad3daf939566d70b46c249dd36743
Nam Luu <nam.luuduc@gmail.com>
2020-08-19 06:09:21 +0700

    add box read content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,25 @@
     <body>
         <header>
             <div class="header-top">
-                <div class="container">header top</div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">header top</div>
+                        <div class="col-md-6 text-right">
+                            @if (Route::has('login'))
+                                <div class="top-right links">
+                                    @auth
+                                        <a href="{{ url('/home') }}">Home</a>
+                                    @else
+                                        <a href="{{ route('login') }}">Login</a>
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}">Register</a>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="header-logo clearfix">
                 <div class="container">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
+<div class="row box-read">
+    <div class="col-md-8">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="card box-content">
+                    <a href=""><img src="https://cdn.tuoitre.vn/zoom/504_315/2020/8/17/img2407-1597662527544697891788-crop-15977549623611233654239.jpg" class="card-img-top" alt="..."></a>
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="">Bệnh nhân 418 tử vong sau 4 lần âm tính COVID-19</a></h5>
+                        <p class="card-text">TTO - Tối 18-8, Bộ phận thường trực đặc biệt chống dịch COVID-19 của Bộ Y tế tại TP Đà Nẵng thông tin về trường hợp bệnh nhân số 418.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <ul class="box-news">
+                    <li><a href="">Tập trận Vành đai Thái Bình Dương tập trung vào chiến đấu trong phạm vi hàng hải</a></li>
+                    <li><a href="">Nhật yêu cầu Trung Quốc kiềm chế quân sự ở Biển Đông và Hoa Đông</a></li>
+                    <li><a href="">Cấm thì cấm, một trường ở Biên Hòa vẫn bắt 800 học sinh đi học</a></li>
+                    <li><a href="">Việt Nam 7 bệnh nhân COVID-19 mới, 0h đêm 19-8 Hà Nội giãn cách tại quán bia, cà phê</a></li>
+                    <li><a href="">Vàng thế giới vượt 2.000 USD/ounce, vàng trong nước thụt lùi vì... ế</a></li>
+                    <li><a href="">Hạ Leipzig 3-0, PSG lần đầu vào chung kết Champions League</a></li>
+                </ul>
+            </div>
         </div>
-    @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card-deck box-focus">
+                    <div class="card">
+                        <a href="">
+                            <img src="https://cdn.tuoitre.vn/zoom/170_106/2020/8/18/11744531915672640834838265544603872446441877n-15977620389751123255268-crop-15977629860601394104578.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Bé trai sơ sinh bị vứt bỏ trong khe tường rất hẹp giữa hai nhà</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card">
+                        <a href="">
+                            <img src="https://cdn.tuoitre.vn/zoom/170_106/2020/8/18/nuoc-lu-cham-chan-lac-son-dai-phat-2-15977472653021302772234-crop-1597747416055333567645.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Lũ bùn đã lên tới chân tượng Phật khổng lồ 1.000 năm tuổi ở Trung Quốc</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card">
+                        <a href="">
+                            <img src="https://cdn.tuoitre.vn/zoom/170_106/2020/8/18/sua-bot-tre-em-15977327460132228466-crop-15977329893851969407405.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Phát hiện 9 loại sữa bột trẻ em chứa chất gây ung thư tại Hong Kong</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card">
+                        <a href="">
+                            <img src="https://cdn.tuoitre.vn/zoom/170_106/2020/8/18/sua-bot-tre-em-15977327460132228466-crop-15977329893851969407405.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Phát hiện 9 loại sữa bột trẻ em chứa chất gây ung thư tại Hong Kong</h5>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 box-ads">
+        <div class="card">
+            <img src="https://tpc.googlesyndication.com/simgad/10594551324937314058" class="card-img-top" alt="...">
+        </div>
+        <div class="card">
+            <img src="https://static-cmsads.admicro.vn/cmsads/2020/07/ttrv-1593774201009.jpeg" class="card-img-top" alt="...">
+        </div>
+    </div>
+</div>
 
+<hr>
+
+<div class="row">
+    <div class="col-md-6"></div>
+    <div class="col-md-6"></div>
+</div>
+
+<div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title m-b-md">
             Laravel
         </div>
-
         <div class="links">
             <a href="https://laravel.com/docs">Docs</a>
             <a href="https://laracasts.com">Laracasts</a>
